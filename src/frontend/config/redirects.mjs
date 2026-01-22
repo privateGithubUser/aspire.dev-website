62ee305bd258f1f0bc73df6ca1d53793750f1274
Alistair Matthews <alistair.matthews@civica.co.uk>
2026-01-22 16:25:08 +0000

    Removed Python and JavaScript pivots from Oracle docs and added a redirect. (#271)

--- src/frontend/config/redirects.mjs
+++ src/frontend/config/redirects.mjs
@@ -15,5 +15,6 @@ export const redirects = {
   '/integrations/databases/efcore/mssql/': '/integrations/databases/efcore/mysql/mysql-get-started/',
   '/integrations/databases/mongodb/': '/integrations/databases/mongodb/mongodb-get-started/',
   '/integrations/databases/mongodb-extensions/': '/integrations/databases/mongodb/mongodb-extensions/',
+  '/integrations/databases/oracle-ef/': '/integrations/databases/efcore/oracle/oracle-get-started/',
   '/integrations/databases/elasticsearch/': '/integrations/databases/elasticsearch/elasticsearch-get-started/',
 };
